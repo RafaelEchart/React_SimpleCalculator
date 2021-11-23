b0312620c64289912edb0851978b96881f912ccf
Rafael Echart <rafaelechartsaltos@gmail.com>
2021-11-23 18:15:19 -0500

    Change functional component to class component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import React from 'react';
 import Calculator from './components/calculator';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Calculator />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Calculator />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
