551004ce27d652cea605c90a54719aecab60c71f
Rafael Echart <rafaelechartsaltos@gmail.com>
2021-11-23 17:43:34 -0500

    Linter tests

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,62 +1,63 @@
-import React from "react";
-import './calculator.css'
+import React from 'react';
+import './calculator.css';
 
 class Calculator extends React.Component {
-    render() {
-      return (
-        <div className="calculator_container">
-          
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="calculator_container">
+
         <div className="result_container">
           000
         </div>
 
         <div className="row_container">
-        <button className="number gray_button">AC</button>
+          <button className="number gray_button" type="button">AC</button>
 
-        <button className="operation">+/-</button>
-        <button className="operation">%</button>
-        <button className="operation">÷</button>
+          <button className="operation" type="button">+/-</button>
+          <button className="operation" type="button">%</button>
+          <button className="operation" type="button">÷</button>
         </div>
 
-        <div className="row_container">
-        
-        <button className="number">7</button>
-        <button className="number">8</button>
-        <button className="number">9</button>
-        <button className="operation">x</button>
-        
-        </div>
-        <div className="row_container">
-        
-        <button className="number">4</button>
-        <button className="number">5</button>
-        <button className="number">6</button>
-        <button className="operation">-</button>
-        
+        <div className="row_container" type="button">
+
+          <button className="number" type="button">7</button>
+          <button className="number" type="button">8</button>
+          <button className="number" type="button">9</button>
+          <button className="operation" type="button">x</button>
+
         </div>
         <div className="row_container">
-        
-        <button className="number">1</button>
-        <button className="number">2</button>
-        <button className="number">3</button>
-        <button className="operation">+</button>
-        
+
+          <button className="number" type="button">4</button>
+          <button className="number" type="button">5</button>
+          <button className="number" type="button">6</button>
+          <button className="operation" type="button">-</button>
+
         </div>
         <div className="row_container">
-        
-        <button className="number_zero">0</button>
-        <button className="number">.</button>
-        <button className="operation">=</button>
-        
-        </div>
 
-     
-
+          <button className="number" type="button">1</button>
+          <button className="number" type="button">2</button>
+          <button className="number" type="button">3</button>
+          <button className="operation" type="button">+</button>
 
         </div>
-      )
-    }
+        <div className="row_container">
+
+          <button className="number_zero" type="button">0</button>
+          <button className="number" type="button">.</button>
+          <button className="operation" type="button">=</button>
+
+        </div>
+
+      </div>
+    );
   }
-  
+}
 
-export default Calculator
+export default Calculator;
